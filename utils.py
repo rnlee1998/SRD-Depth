d09f0ed4b139a7dddf336ac920ef84f16016554e
rnlee1998 <1050145004@qq.com>
2022-12-14 11:14:40 +0800

    update utils.py

--- utils.py
+++ utils.py
@@ -5,12 +5,18 @@
 # available in the LICENSE file.
 
 from __future__ import absolute_import, division, print_function
+from torch import nn
+from inverse_warp import inverse_warp2, inverse_warp
+import math
 import os
 import hashlib
 import zipfile
 from six.moves import urllib
 import torch
 import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import torch.nn.functional as F
 def readlines(filename):
     """Read all the lines in a text file and return as a list
     """
@@ -142,3 +148,63 @@ def colormap(inputs, normalize=True, torch_transpose=True):
             vis = vis.transpose(2, 0, 1)
 
     return vis[0,:,:,:]
+
+# project the reference point cloud into the source view, then project back
+def reproject_with_depth(depth_ref, intrinsics_ref, depth_src, intrinsics_src, T):
+    width, height = depth_ref.shape[1], depth_ref.shape[0]
+    ## step1. project reference pixels to the source view
+    # reference view x, y
+    x_ref, y_ref = np.meshgrid(np.arange(0, width), np.arange(0, height))
+    x_ref, y_ref = x_ref.reshape([-1]), y_ref.reshape([-1]) 
+
+    # reference 3D space
+    xyz_ref = np.matmul(np.linalg.inv(intrinsics_ref),
+                        np.vstack((x_ref, y_ref, np.ones_like(x_ref))) * depth_ref.reshape([-1]))
+    # source 3D space
+    xyz_src = np.matmul(T,np.vstack((xyz_ref, np.ones_like(x_ref))))[:3]
+    # source view x, y
+    K_xyz_src = np.matmul(intrinsics_src, xyz_src)
+    xy_src = K_xyz_src[:2] / K_xyz_src[2:3]
+
+    ## step2. reproject the source view points with source view depth estimation
+    # find the depth estimation of the source view
+    x_src = xy_src[0].reshape([height, width]).astype(np.float32)
+    y_src = xy_src[1].reshape([height, width]).astype(np.float32)
+    sampled_depth_src = cv2.remap(depth_src, x_src, y_src, interpolation=cv2.INTER_LINEAR)
+    # mask = sampled_depth_src > 0
+
+    # source 3D space
+    # NOTE that we should use sampled source-view depth_here to project back
+    xyz_src = np.matmul(np.linalg.inv(intrinsics_src),
+                        np.vstack((xy_src, np.ones_like(x_ref))) * sampled_depth_src.reshape([-1]))
+    # reference 3D space
+    xyz_reprojected = np.matmul(np.linalg.inv(T),np.vstack((xyz_src, np.ones_like(x_ref))))[:3]
+    # source view x, y, depth
+    depth_reprojected = xyz_reprojected[2].reshape([height, width]).astype(np.float32)
+    K_xyz_reprojected = np.matmul(intrinsics_ref, xyz_reprojected)
+    xy_reprojected = K_xyz_reprojected[:2] / K_xyz_reprojected[2:3]
+    x_reprojected = xy_reprojected[0].reshape([height, width]).astype(np.float32)
+    y_reprojected = xy_reprojected[1].reshape([height, width]).astype(np.float32)
+
+    return depth_reprojected, x_reprojected, y_reprojected, x_src, y_src
+
+
+def check_geometric_consistency(depth_ref, intrinsics_ref, depth_src, intrinsics_src, T):
+    width, height = depth_ref.shape[1], depth_ref.shape[0]
+    x_ref, y_ref = np.meshgrid(np.arange(0, width), np.arange(0, height))
+    depth_reprojected, x2d_reprojected, y2d_reprojected, x2d_src, y2d_src = reproject_with_depth(depth_ref, intrinsics_ref, 
+                                                                                                depth_src,intrinsics_src, 
+                                                                                                T)
+    # check |p_reproj-p_1| < 1
+    dist = np.sqrt((x2d_reprojected - x_ref) ** 2 + (y2d_reprojected - y_ref) ** 2)
+
+    # check |d_reproj-d_1| / d_1 < 0.01
+    depth_diff = np.abs(depth_reprojected - depth_ref)
+    relative_depth_diff = depth_diff / depth_ref                    
+    # mask = np.logical_and(dist < dist.mean(), relative_depth_diff < depth_diff.mean())
+    mask =  relative_depth_diff < 0.01
+    depth_reprojected[~mask] = 0
+
+    return mask, depth_reprojected, x2d_src, y2d_src
+
+
